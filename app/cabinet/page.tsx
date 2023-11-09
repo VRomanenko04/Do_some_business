aa8219d239d963fdc1d0eaf1a5218f9da66e1826
VRomanenko04 <vovasan2004@gmail.com>
2023-11-09 19:39:14 +0200

    Сделал блочную структуру для Cabinet, внёс правки в NavBar и global styles

--- app/cabinet/page.tsx
+++ app/cabinet/page.tsx
@@ -1,16 +1,35 @@
 import NavBar from '@/components/Navigation/NavBar';
-import React from 'react'
+import React from 'react';
+import styles from './Cabinet.module.scss';
 
 
 const CabinetPage = () => {
-
     return (
         <>
             <header>
                 <NavBar />
             </header>
-            <main>
-                <div>Cabinet Page</div>
+            <main className={styles.main}>
+                <section className={styles.wrapper}>
+                    <div className={styles.up__container}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.small_cont}>
+                            <p>Name: Vladimir</p>
+                            <p>E-mail: ...</p>
+                        </div>
+                        <div className={styles.small_cont}>
+                            <p>Age: 19</p>
+                            <p>Gender: Male</p>
+                        </div>
+                    </div>
+                </section>
+                <section className={styles.wrapper}>
+                    <div className={styles.down__container}>
+                        <p>Projects in work: ...</p>
+                        <p>Financial condition: ...</p>
+                        <p>Place in iForbes: ...</p>
+                    </div>
+                </section>
             </main>
         </>
     )
