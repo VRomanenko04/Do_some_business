685507b2dbc69abb6b0242c4dc4447572097cd05
VRomanenko04 <vovasan2004@gmail.com>
2023-11-11 16:12:27 +0200

    Убрал тестовый redux slice

--- app/globalRedux/store.ts
+++ app/globalRedux/store.ts
@@ -1,11 +1,9 @@
 'use client';
 
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from './Features/counterSlice';
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
     }
 })
 
