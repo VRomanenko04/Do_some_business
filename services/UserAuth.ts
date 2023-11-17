01fcd2810b755974d40056937a5563026f11be5f
VRomanenko04 <vovasan2004@gmail.com>
2023-11-17 21:10:58 +0200

    Усовершенствовал сервис UserRegister для связи с БД

--- services/UserAuth.ts
+++ services/UserAuth.ts
@@ -1,16 +1,24 @@
-import { firebaseAuth } from "@/app/firebase";
+import { firebaseAuth, firebaseDatabase } from "@/app/firebase";
 import { IUser } from "@/app/globalRedux/Features/userAuth.slice";
 import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { ref, set } from "firebase/database";
 
 export const UserRegister = async (email: string, password: string, setUser: ActionCreatorWithPayload<IUser, "userAuth/setUser">) => {
     try {
         const { user } = await createUserWithEmailAndPassword(firebaseAuth, email, password);
 
-        setUser({
-            email: user.email,
-            uid: user.uid,
-        });
+        const userRef = ref(firebaseDatabase, 'users/' + user.uid);
+        const userData = {
+            email: email,
+        }
+        await set(userRef, userData).then(() => {
+            setUser({
+                email: user.email,
+                uid: user.uid,
+            });
+        })
+
     } catch (error: any) {
         console.log(error);
     }
